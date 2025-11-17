86af37d975e58facf649c314a805d08ec65bd7b1
BaconEggsRL <apatton090@gmail.com>
2025-11-17 18:56:34 -0500

    asd

--- test/spotlight.gd
+++ test/spotlight.gd
@@ -9,6 +9,20 @@ extends Node2D
 @onready var light: PointLight2D = $light
 
 
+@export var preview_flicker:bool = false:
+	set(value):
+		preview_flicker = value
+		if value:
+			start_preview_flicker()
+		else:
+			stop_preview_flicker()
+			
+@export var should_flicker:bool = false
+@export var flicker_time:float = 1.0
+@onready var flicker_timer: Timer = $FlickerTimer
+
+
+
 @export_range(0.0, 512.0, 1.0) var offset:float = 128.0:
 	set(value):
 		offset = value
@@ -40,9 +54,28 @@ var starting_rotation:float
 
 
 
+func enable_light() -> void:
+	light.enabled = true
+	for ray:RayCast2D in ray_children:
+		ray.enabled = true
+
+func disable_light() -> void:
+	for ray:RayCast2D in ray_children:
+		ray.enabled = false
+	light.enabled = false
+
+func toggle_light() -> void:
+	if light.enabled == true:
+		disable_light()
+	else:
+		enable_light()
+
+
+
 func _ready() -> void:
 	update_light()
 	start_preview_rotation()
+	start_preview_flicker()
 
 
 func update_light() -> void:
@@ -51,6 +84,20 @@ func update_light() -> void:
 		rays.position.y = -offset - 64.0
 
 
+func start_preview_flicker() -> void:
+	if Engine.is_editor_hint():
+		if not preview_flicker:
+			return
+		else:
+			flicker_timer.wait_time = self.flicker_time
+	if should_flicker:
+		add_flicker()
+	
+func stop_preview_flicker() -> void:
+	flicker_timer.stop()
+	light.enabled = true
+	
+	
 func start_preview_rotation() -> void:
 	if Engine.is_editor_hint():
 		if not preview_rotate:
@@ -77,7 +124,10 @@ func add_rotation() -> void:
 	else:
 		light_tween.tween_property(self, "rotation_degrees", self.rotation_degrees - rotate_amount, rotate_time)
 		light_tween.tween_property(self, "rotation_degrees", self.rotation_degrees + rotate_amount, rotate_time)
-	
+
+
+func add_flicker() -> void:
+	flicker_timer.start()
 
 
 func add_reflector(r: Node) -> void:
@@ -97,22 +147,34 @@ func clear_reflectors() -> void:
 	
 
 func _physics_process(_delta):
-	for ray:RayCast2D in ray_children:
+	var hit_reflectors := {}  # Keep track of reflectors hit this frame
+
+	for ray: RayCast2D in ray_children:
+		if not ray.enabled:
+			continue
 		
 		var collider = ray.get_collider()
+		if collider and collider.is_in_group("reflector_head"):
+			var r = collider.get_parent()
+			hit_reflectors[r] = true  # mark as hit
 
+			if not has_reflector(r):
+				add_reflector(r)
+		
+		# Damage stuff
 		if collider:
-
-			if collider.is_in_group("reflector_head"):
-				var r = collider.get_parent()
-				if not has_reflector(r):
-					add_reflector(r)
-			
 			if collider.is_in_group("player") and collider is Player:
 				collider.hp -= beam_dps * _delta
-
 			if collider.is_in_group("umbrella") and collider is Umbrella:
 				collider.hp -= beam_dps * _delta
-			
-		else:
-			clear_reflectors()
+	
+	# Remove reflectors that are no longer hit
+	for r in active_reflectors.keys():
+		if not hit_reflectors.has(r):
+			remove_reflector(r)
+
+
+func _on_flicker_timer_timeout() -> void:
+	toggle_light()
+	if should_flicker:
+		flicker_timer.start()
