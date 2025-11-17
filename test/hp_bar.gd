940e6f9b1cbaa78fd8772e9af7d4bb6f47c2366c
BaconEggsRL <apatton090@gmail.com>
2025-11-17 14:58:52 -0500

    asd

--- test/hp_bar.gd
+++ test/hp_bar.gd
@@ -15,8 +15,23 @@ func setup(node:Node) -> void:
 	entity.hp_changed.connect(_on_hp_changed)
 	
 func _on_hp_changed(new_value) -> void:
+	# Kill existing tween
 	if tween:
 		tween.kill()
-	tween = create_tween()
-	tween.tween_property(self, "value", new_value, 0.25)  # duration = 0.2 sec
+		
+	# How big is the HP change?
+	var delta = abs(new_value - value)
+	var _range = max_value - min_value
+
+	# Normalize change into 0.0 → 1.0
+	var t := 0.0
+	if _range > 0:
+		t = delta / _range
+
+	# Scale tween duration: small change = fast, big change = slow
+	var max_time = 1.0
+	var duration = min(max_time, t * max_time)  # max 0.5 sec
+	
+	tween = create_tween().set_ease(Tween.EASE_OUT)# .set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(self, "value", new_value, duration)  # duration = 0.2 sec
 	# self.value = new_value
