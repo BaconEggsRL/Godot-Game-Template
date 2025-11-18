8a83e7d42d73fa0c674d49bc03bd7451c038b1ce
BaconEggsRL <apatton090@gmail.com>
2025-11-17 20:40:21 -0500

    asd

--- test/wind.gd
+++ test/wind.gd
@@ -33,24 +33,27 @@ func check_ray_collisions(_delta) -> void:
 			var collider = ray.get_collider()
 
 			# if not umbrella, block/reduce height of air stream
+			var ray_collision_height = max_height
+			
 			if collider is not Umbrella:
 				var ray_hit_global: Vector2 = ray.get_collision_point()
 				var ray_hit_local = ray.to_local(ray_hit_global)
-				var ray_collision_height = ray_hit_local.y  # <-- global Y of the collision
-				print(ray_collision_height)
+				ray_collision_height = ray_hit_local.y  # <-- global Y of the collision
 				
-				
-				pass
-				
+			current_height = ray_collision_height
+			# print(current_height)
+			
 
-			# Only push if the player is ABOVE the collision point
-			#if player.global_position.y > ray_collision_height:
-				#
-				#player.wind_velocity = Vector2(0, -PUSH_FORCE)
-				#if recharge:
-					#umbrella.hp += recharge_dps * _delta
-				#else:
-					#player.wind_velocity = Vector2.ZERO
+			# Only push if the player is BELOW the current height
+			var global_height = ray.to_global(Vector2(0, current_height)).y
+
+			if player.global_position.y > global_height:
+				
+				player.wind_velocity = Vector2(0, -PUSH_FORCE)
+				if recharge:
+					umbrella.hp += recharge_dps * _delta
+				else:
+					player.wind_velocity = Vector2.ZERO
 
 			return  # Stop after first ray hit
 
