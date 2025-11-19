39ee479f943997994fa5faeee2fbd20cbb858081
BaconEggsRL <apatton090@gmail.com>
2025-11-19 12:12:53 -0500

    asd

--- test/trap_door.gd
+++ test/trap_door.gd
@@ -5,6 +5,8 @@ extends Node2D
 @onready var left_collision: CollisionShape2D = $pivot_left/left/left_collision
 @onready var right_collision: CollisionShape2D = $pivot_right/right/right_collision
 
+@export var anim_speed:float = 1.0
+
 
 @export var enabled:bool = false:  # closed if false
 	set(value):
@@ -21,14 +23,18 @@ func _ready() -> void:
 		set_collision(true)
 
 func open_door() -> void:
-	anim.play("open")
+	var reverse := false
+	anim.play("open", -1, anim_speed, reverse)
 	set_collision(false)
+	#if not anim.animation_finished.is_connected(_on_opened):
+		#anim.animation_finished.connect(_on_opened, CONNECT_ONE_SHOT)
 
 func close_door() -> void:
 	var reverse := true
-	anim.play("open", -1, -1.0, reverse)
+	anim.play("open", -1, -1.0 * anim_speed, reverse)
 	# set_collision(true)
-	anim.animation_finished.connect(_on_closed, CONNECT_ONE_SHOT)
+	if not anim.animation_finished.is_connected(_on_closed):
+		anim.animation_finished.connect(_on_closed, CONNECT_ONE_SHOT)
 
 func set_collision(collision:bool) -> void:
 	left_collision.disabled = not collision
@@ -36,3 +42,6 @@ func set_collision(collision:bool) -> void:
 
 func _on_closed(_anim_name:String) -> void:
 	set_collision(true)
+
+func _on_opened(_anim_name:String) -> void:
+	set_collision(false)
