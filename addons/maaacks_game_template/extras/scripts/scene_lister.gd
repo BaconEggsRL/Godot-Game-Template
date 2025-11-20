4be2914b75cbaced56fc076f01c9596f709f974b
BaconEggsRL <apatton090@gmail.com>
2025-11-20 18:31:35 -0500

    asd

--- addons/maaacks_game_template/extras/scripts/scene_lister.gd
+++ addons/maaacks_game_template/extras/scripts/scene_lister.gd
@@ -1,26 +1,47 @@
 @tool
 extends Node
 class_name SceneLister
-## Helper class for listing all the scenes in a directory.
 
-## List of paths to scene files.
-## Prefilled in the editor by selecting a directory.
 @export var files : Array[String]
-## Prefill files with any scenes in the directory.
+
 @export_dir var directory : String = "res://scenes/game_scene/levels":
 	set(value):
 		directory = value
-		_refresh_files()
+		if Engine.is_editor_hint():
+			_refresh_files()
 
-func _ready() -> void:
-	_refresh_files()
-	
 func _refresh_files():
-	if not is_inside_tree() or directory.is_empty(): return
 	var dir_access = DirAccess.open(directory)
-	if dir_access:
-		files.clear()
-		for file in dir_access.get_files():
-			if not file.ends_with(".tscn"):
-				continue
+	if not dir_access: return
+	files.clear()
+	for file in dir_access.get_files():
+		if file.ends_with(".tscn"):
 			files.append(directory + "/" + file)
+
+
+#@tool
+#extends Node
+#class_name SceneLister
+### Helper class for listing all the scenes in a directory.
+#
+### List of paths to scene files.
+### Prefilled in the editor by selecting a directory.
+#@export var files : Array[String]
+### Prefill files with any scenes in the directory.
+#@export_dir var directory : String = "res://scenes/game_scene/levels":
+	#set(value):
+		#directory = value
+		#_refresh_files()
+#
+#func _ready() -> void:
+	#_refresh_files()
+	#
+#func _refresh_files():
+	#if not is_inside_tree() or directory.is_empty(): return
+	#var dir_access = DirAccess.open(directory)
+	#if dir_access:
+		#files.clear()
+		#for file in dir_access.get_files():
+			#if not file.ends_with(".tscn"):
+				#continue
+			#files.append(directory + "/" + file)
